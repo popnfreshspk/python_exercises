673341ad84ace01a621355dab882c62fe65859e7
kevinhsu <kevinhsu@Kevins-MBP.fios-router.home>
2017-04-01 21:32:29 -0400

    Adding part 2 for tic-tac-toe (userinput). Renaming exercises to be more descriptive

---
+++ tic_tac_toe/pt2_userinput.py
@@ -0,0 +1,45 @@
+# TIC-TAC-TOE Part 2:
+# 
+# After finishing part 1, you should have a function that draws the game board.
+# The goal for part 2 is to give users the ability to place their pieces 'x' or 'o'
+# in the positions (0-8) on the game baord
+#
+# The insert_move() funciton should take a user input and replace the game board 
+# with a tic-tac-toe piece: 
+#
+#  0 | 1 | 2      0 | 1 | 2
+# --- --- ---    --- --- ---
+#  3 | 4 | 5  =>  3 | x | 5  
+# --- --- ---    --- --- ---
+#  6 | 7 | 8      6 | 7 | 8
+#
+# You can use raw_input() function to get the user's input.  
+#
+def insert_move():
+	return 0
+
+def print_board(game_board):
+	#
+	# Your working code goes here
+	#
+	return 0
+
+
+
+# This is the primary game loop, you will run the tic-tac-toe game using this
+def tic_tac_toe():
+
+	game_board = [
+		[None, None, None],
+		[None, None, None],
+		[None, None, None]
+	]
+
+	print_board(game_board)
+	
+	#
+	# Call insert_move() here and the print the gameboard again 
+	# to test and make sure the the move was saved properly
+	#
+
+	print_board(game_board)
